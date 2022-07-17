6ff7c617d895659a254c096ef79e6a992085e678
yangyun <1410432625@qq.com>
2022-07-17 18:00:08 +0800

    update footer title

--- site/start.sh
+++ site/start.sh
@@ -5,4 +5,4 @@ set -eu
 docker stop bbssites && docker rm bbssites
 docker system prune -a
 docker build -t bbssite .
-docker run -itd --name bbsites -p 3000:3000 --restart=always bbssite:latest
+docker run -itd --name bbssites -p 3000:3000 --restart=always bbssite:latest
